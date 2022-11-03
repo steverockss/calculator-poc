b451c33f751893a1b194aa834a3ebf7e520d95e9
stevenarevalo <steven.arevalo@mercadolibre.com.co>
2022-11-03 12:12:04 -0500

    Added test to the controller

---
+++ src/test/java/com/soprasteriatest/calculator/CalculatorControllerTest.java
@@ -0,0 +1,73 @@
+package com.soprasteriatest.calculator;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.soprasteriatest.calculator.controller.CalculatorController;
+import com.soprasteriatest.calculator.dto.CalculatorRequest;
+import com.soprasteriatest.calculator.dto.CalculatorResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class CalculatorControllerTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private  final CalculatorController calculatorController;
+
+    @Autowired
+    public CalculatorControllerTest(CalculatorController calculatorController) {
+        this.calculatorController = calculatorController;
+    }
+
+    @Test
+    void testShouldAddTwoNumbersSuccessfully() {
+        CalculatorRequest request = new CalculatorRequest(3, 4);
+        CalculatorResponse response = calculatorController.addition(request);
+        System.out.println(response.getResult());
+    }
+
+    @Test
+    void shouldAddSuccessfully() throws Exception {
+        CalculatorRequest request = new CalculatorRequest(3, 4);
+        String requestJSON = objectMapper.writeValueAsString(request);
+       mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/calculator/add").contentType(MediaType.APPLICATION_JSON).content(requestJSON)).andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReturn400DueEmptyParams() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/calculator/add").contentType(MediaType.APPLICATION_JSON).content("")).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void shouldReturn404DueBadPath() throws Exception {
+        CalculatorRequest request = new CalculatorRequest(3, 4);
+        String requestJSON = objectMapper.writeValueAsString(request);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/calculator/addition").contentType(MediaType.APPLICATION_JSON).content(requestJSON)).andExpect(status().isNotFound());
+    }
+
+    @Test
+    void shouldReturn500DueExceptionCausedByDivisionByZero() throws Exception {
+        CalculatorRequest request = new CalculatorRequest(0, 0);
+        String requestJSON = objectMapper.writeValueAsString(request);
+        assertThrows(Exception.class, () -> {
+            mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/calculator/divide").contentType(MediaType.APPLICATION_JSON).content(requestJSON)).andExpect(status().isInternalServerError());
+
+        });
+    }
+}
--- src/test/java/com/soprasteriatest/calculator/CalculatorServiceTest.java
+++ src/test/java/com/soprasteriatest/calculator/CalculatorServiceTest.java
@@ -1,7 +1,6 @@
 package com.soprasteriatest.calculator;
 
 import com.soprasteriatest.calculator.service.CalculatorServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
