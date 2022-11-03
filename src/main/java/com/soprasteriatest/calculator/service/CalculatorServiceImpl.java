e91faf82dba0ff386fd04b071beeacc8d8019015
stevenarevalo <steven.arevalo@mercadolibre.com.co>
2022-11-03 10:47:39 -0500

    Added trace to the service methods

--- src/main/java/com/soprasteriatest/calculator/service/CalculatorServiceImpl.java
+++ src/main/java/com/soprasteriatest/calculator/service/CalculatorServiceImpl.java
@@ -1,13 +1,23 @@
 package com.soprasteriatest.calculator.service;
 
+import io.corp.calculator.TracerImpl;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class CalculatorServiceImpl  implements  CalculatorService{
+
+    private final TracerImpl tracer;
     @Override
     public double add(double num1, double num2) {
+        tracer.trace("Adding "+ num1+ " and "+ num2);
+        tracer.trace("Result:: "+ (num1+ num2));
         return num1+num2;
     }
 
     @Override
     public double subtract(double num1, double num2) {
+        tracer.trace("Subtracting "+ num1+ " and "+ num2);
+        tracer.trace("Result:: "+ (num1 - num2));
         return num1-num2;
     }
 }
